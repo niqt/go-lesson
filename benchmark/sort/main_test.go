1af7b61aa198d03060105d20c49e116214599523
Nicola De Filippo <nicola@nicoladefilippo.it>
2024-11-16 18:38:03 +0100

    Added instructions

--- benchmark/sort/main_test.go
+++ benchmark/sort/main_test.go
@@ -1,5 +1,8 @@
 package main
 
+// Run with GOGC=off go test -cpu 8 -run none -bench . -benchtime 3s
+// Run with GOGC=off go test -cpu 1 -run none -bench . -benchtime 3s
+
 import (
 	"math/rand"
 	"runtime"
